c8fdfcf5cb35a6871979ec2a9ee33c43e4768fb4
Ruben Gomez <rubncal.9501@gmail.com>
2024-10-05 11:02:01 -0500

    Copy of main

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,25 @@
 class ContactsController < ApplicationController
+  # before_action :set_contact, only: [:show, :edit, :update, :destroy]
+
   def index
     @contacts_by_city = Contact.joins(:city).select("contacts.*, cities.name as city_name").group_by(&:city_name)
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new contact_params
+  end
+  
+  private
+  def set_contact
+    @image = Contact.find params[:id]
+  end
+
+  def contact_params
+    params.require(:contact).permit :name, :last_name, :email,
+    :city_id, :address, :birthday, :gender
   end
 end
